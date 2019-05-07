c06a4d041cb170d61ee6574df4465dcf62ab5a56
Christopher Adamson <software.pro.08@gmail.com>
2019-05-07 16:38:36 -0700

    test commit

---
+++ 01-SELECT_basics.sql
@@ -0,0 +1,6 @@
+
+The example uses a WHERE clause to show the population of 'France'. Note that strings (pieces of text that are data) should be in 'single quotes';
+
+SELECT population FROM world
+  WHERE name = 'Germany'
+
